3a67c2621c77b31faccea03bddd1b4e5338961bc
jmisurda <jmisurda@localhost>
2025-10-16 00:31:40 -0700

    Initial Project Commit

---
+++ src/TemperatureConverter.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TemperatureConverter extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+
+}
